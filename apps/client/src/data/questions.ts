02de7a1c81c834a62c5f92b6230d297219ce06b5
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2025-01-19 07:13:28 -0800

    fixed questions

--- apps/client/src/data/questions.ts
+++ apps/client/src/data/questions.ts
@@ -23,31 +23,31 @@ export const questions =
   {
     title: "colour",
     items: [
-      { id: 1, name: "Pancakes", image: "pancake-icon.png" },
-      { id: 2, name: "Waffles", image: "waffle-icon.png" },
-      { id: 3, name: "Cake", image: "cake-icon.png" },
-      { id: 4, name: "Cupcake", image: "cupcake-icon.png" },
-      { id: 5, name: "Cookie", image: "cookie-icon.png" },
+      { id: 1, name: "Red", image: "pancake-icon.png" },
+      { id: 2, name: "Blue", image: "waffle-icon.png" },
+      { id: 3, name: "Green", image: "cake-icon.png" },
+      { id: 4, name: "Yellow", image: "cupcake-icon.png" },
+      { id: 5, name: "Purple", image: "cookie-icon.png" },
     ]
   },
   {
     title: "sport",
     items: [
-      { id: 1, name: "Pancakes", image: "pancake-icon.png" },
-      { id: 2, name: "Waffles", image: "waffle-icon.png" },
-      { id: 3, name: "Cake", image: "cake-icon.png" },
-      { id: 4, name: "Cupcake", image: "cupcake-icon.png" },
-      { id: 5, name: "Cookie", image: "cookie-icon.png" },
+      { id: 1, name: "Soccer", image: "pancake-icon.png" },
+      { id: 2, name: "Hockey", image: "waffle-icon.png" },
+      { id: 3, name: "Basketball", image: "cake-icon.png" },
+      { id: 4, name: "Running", image: "cupcake-icon.png" },
+      { id: 5, name: "Swimming", image: "cookie-icon.png" },
     ]
   },
   {
     title: "superhero",
     items: [
-      { id: 1, name: "Pancakes", image: "pancake-icon.png" },
-      { id: 2, name: "Waffles", image: "waffle-icon.png" },
-      { id: 3, name: "Cake", image: "cake-icon.png" },
-      { id: 4, name: "Cupcake", image: "cupcake-icon.png" },
-      { id: 5, name: "", image: "cookie-icon.png" },
+      { id: 1, name: "Superman", image: "pancake-icon.png" },
+      { id: 2, name: "Batman", image: "waffle-icon.png" },
+      { id: 3, name: "Spiderman", image: "cake-icon.png" },
+      { id: 4, name: "Iron Man", image: "cupcake-icon.png" },
+      { id: 5, name: "Wonder Woman", image: "cookie-icon.png" },
     ]
   }
 ];
