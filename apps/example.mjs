0e9fca92ec18c23eab8f468fc0254cc5615f6ce3
enora <enorasun1120@gmail.com>
2025-01-18 19:49:41 -0800

    downloaded axios

--- apps/example.mjs
+++ apps/example.mjs
@@ -17,14 +17,28 @@ const textCompletion = await openai.chat.completions.create({
 
 console.log(textCompletion.choices[0].message);
 
+
 if (textCompletion.choices && textCompletion.choices.length > 0 && textCompletion.choices[0].message) {
-    let promptImageBody = textCompletion.choices[0].message.content; 
-    let refinedPrompt = `${promptImageBody} (illustration without text)`;
+    let promptBody = textCompletion.choices[0].message.content; 
+    let refinedImagePrompt = `${promptBody} (illustration without text)`;
+
+    const translationCompletion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            { role: "system", content: "You are a worldly-announced translator." },
+            {
+                role: "user",
+                content: `Translate ${promptBody} to mandarin.`,
+            },
+        ],
+    });
+    
+    console.log(translationCompletion.choices[0].message);
 
     // Generate image
     const imageResponse = await openai.images.generate({
         model: "dall-e-3",
-        prompt: refinedPrompt,
+        prompt: refinedImagePrompt,
         size: "1024x1024",
         quality: "standard",
         n: 1,
