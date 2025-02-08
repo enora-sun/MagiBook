2132af403f3b3b4824f7e803d2b36ebf3e0c386d
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2025-02-08 14:53:36 -0800

    updated cors origin

--- apps/app.js
+++ apps/app.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://magistory.netlify.app',
   methods: 'GET,POST',
   allowedHeaders: 'Content-Type'
 }));
